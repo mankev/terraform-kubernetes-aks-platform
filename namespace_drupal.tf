2072c6705e4b5b7dcaf6acf65057cdd80c66ef5c
Heisenberg <tutlewax@gmail.com>
2019-10-31 21:50:17 -0400

    remove comments

--- namespace_drupal.tf
+++ namespace_drupal.tf
@@ -1,44 +1,44 @@
 # Drupal
 
-# resource "kubernetes_namespace" "drupal" {
-#   metadata {
-#     name = "drupal"
+ resource "kubernetes_namespace" "drupal" {
+   metadata {
+     name = "drupal"
 
-#     labels = {
-#       istio-injection = "enabled"
-#     }
-#   }
-# }
+     labels = {
+       istio-injection = "enabled"
+     }
+   }
+ }
 
-# module "namespace_drupal" {
-#   source = "git::https://github.com/statcan/terraform-kubernetes-namespace.git"
+ module "namespace_drupal" {
+   source = "git::https://github.com/statcan/terraform-kubernetes-namespace.git"
 
-#   name = "${kubernetes_namespace.drupal.metadata.0.name}"
-#   namespace_admins = {
-#     users = []
-#     groups = [
-#       "${var.kubernetes_rbac_group}"
-#     ]
-#   }
+   name = "${kubernetes_namespace.drupal.metadata.0.name}"
+   namespace_admins = {
+     users = []
+     groups = [
+       "${var.kubernetes_rbac_group}"
+     ]
+   }
 
-#   # ServiceAccount
-#   helm_service_account = "tiller"
+   # ServiceAccount
+   helm_service_account = "tiller"
 
-#   # ServiceQuota Overrides
-#   allowed_loadbalancers = "10"
-#   allowed_nodeports = "10"
+   # ServiceQuota Overrides
+   allowed_loadbalancers = "10"
+   allowed_nodeports = "10"
 
-#   # CICD
-#   ci_name = "argo"
+   # CICD
+   ci_name = "argo"
 
-#   # Image Pull Secret
-#   # enable_kubernetes_secret = "${var.enable_kubernetes_secret}"
-#   # kubernetes_secret = "${var.kubernetes_secret}"
-#   # docker_repo = "${var.docker_repo}"
-#   # docker_username = "${var.docker_username}"
-#   # docker_password = "${var.docker_password}"
-#   # docker_email = "${var.docker_email}"
-#   # docker_auth = "${var.docker_auth}"
+   # Image Pull Secret
+   # enable_kubernetes_secret = "${var.enable_kubernetes_secret}"
+   # kubernetes_secret = "${var.kubernetes_secret}"
+   # docker_repo = "${var.docker_repo}"
+   # docker_username = "${var.docker_username}"
+   # docker_password = "${var.docker_password}"
+   # docker_email = "${var.docker_email}"
+   # docker_auth = "${var.docker_auth}"
 
-#   dependencies = []
-# }
+   dependencies = []
+ }
